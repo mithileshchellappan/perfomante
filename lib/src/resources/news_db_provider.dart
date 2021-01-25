67d481207bc700b896b0a1dbbbc1c3391f0e17ae
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-01-25 14:48:24 +0530

    defining item model

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -37,4 +37,17 @@ class NewsDbProvider {
           );
         });
   }
+  fetchItem(int id)async{
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where:"id = ?",
+      whereArgs:[id],
+    );
+
+    if(maps.length>0){
+      return ItemModel.fromDb(maps.first);
+    }
+    return null;
+  }
 }
