260be7572e842008a456d0bd913326f68f5bf587
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-01-25 10:55:58 +0530

    testPush

--- test/src/news_api_provider_test.dart
+++ test/src/news_api_provider_test.dart
@@ -4,17 +4,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
-void main(){
-  test('FetchTopIds returns a list of ids',()async{
+void main() {
+  test('FetchTopIds returns a list of ids', () async {
     final newsApi = NewsApiProvider();
-    
-    newsApi.client = MockClient((request)async{
-      return Response(json.encode([1,2,3,4]), 200);
+
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
     });
 
     final ids = await newsApi.fetchTopIds();
 
-    expect(ids,[1,2,3,4]);
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('fetchitem returns item model', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+    final item = await newsApi.fetchItem(999);
+    expect(item.id,123);
   });
 }
-
