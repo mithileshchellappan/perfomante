f1c58d3b2a0400d8dfa6d720d3ba06831c3a3c24
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-01-25 14:22:33 +0530

    defining db

--- lib/src/resources/news_dp_provider.dart
+++ lib/src/resources/news_dp_provider.dart
@@ -5,3 +5,36 @@ import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
 
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+        path,
+        version: 1, 
+        onCreate: (Database newDb, int version) {
+          newDb.execute(
+            '''CREATE TABLE Items
+              (
+                id INTEGER PRIMARY KEY,
+                type TEXT,
+                by TEXT,
+                time INTEGER,
+                text TEXT,
+                parent INTEGER,
+                kids BLOB,
+                dead INTEGER,
+                deleted INTEGER,
+                url TEXT,
+                score INTEGER,
+                title TEXT,
+                descendants INTEGER
+
+              )
+            '''
+          );
+        });
+  }
+}
