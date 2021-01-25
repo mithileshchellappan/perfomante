74f4e5b5de5316c6fd1b9a1d6f4b41e63c01c5f4
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-01-25 10:51:16 +0530

    tests

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,18 +1,23 @@
 import 'dart:convert';
-
+import'../models/item_model.dart';
 import 'package:http/http.dart' show Client;
+final _root = 'https://hacker-news.firebaseio.com/v0';
 
-class NewsApiProvider{
+class NewsApiProvider {
   Client client = Client();
 
-
-  fetchTopIds ()async{
-    final response =  await client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json');
     final ids = json.decode(response.body);
 
     return ids;
   }
-  fetchItem(){
 
+  fetchItem(int id) async {
+    final response = await client.get(
+        '$_root/item/$id.json?print=pretty');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson);
   }
 }
---
+++ test/src/news_api_provider_test.dart
@@ -0,0 +1,20 @@
+import 'package:perfomante/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main(){
+  test('FetchTopIds returns a list of ids',()async{
+    final newsApi = NewsApiProvider();
+    
+    newsApi.client = MockClient((request)async{
+      return Response(json.encode([1,2,3,4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids,[1,2,3,4]);
+  });
+}
+
