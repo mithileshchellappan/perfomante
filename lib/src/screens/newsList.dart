f0bf99b4f51b8109eef4738924d32eabe23a9af5
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-01-27 14:51:16 +0530

    futurebuilder

--- lib/src/screens/newsList.dart
+++ lib/src/screens/newsList.dart
@@ -7,7 +7,26 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Some News'),
+      body: buildList(),
     );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, index) {
+        return FutureBuilder(
+            future: getFuture(),
+            builder: (context, snapshot) {
+              return snapshot.hasData
+                  ? Text(snapshot.data + index.toString())
+                  : Text(index.toString());
+            });
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(Duration(seconds: 2), () => 'yep im visible');
   }
 }
