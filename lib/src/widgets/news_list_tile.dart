44a2e7e5b82101ab2b505db6a69dcdf9a7137450
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-01-28 18:34:55 +0530

    test_build

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+import 'dart:async';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({this.itemId});
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream Loading');
+        } else {
+          return FutureBuilder(builder: (context, snapshot) {
+            return Text(snapshot.data[0].toString());
+          });
+        }
+      },
+    );
+  }
+}
